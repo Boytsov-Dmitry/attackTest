8648590ca04244ddca9223c4092a9b16952c2b33
Dmitry Boytsov <135224601+Boytsov-Dmitry@users.noreply.github.com>
2024-06-06 09:58:51 +0300

    first

--- src/js/Class/Magician.js
+++ src/js/Class/Magician.js
@@ -1,6 +1,6 @@
 import MainMath from './mainClass';
 
-export default class Magician extends Math {
+export default class Magician extends MainMath {
     constructor() {
         super();
         this.attack = 100;
